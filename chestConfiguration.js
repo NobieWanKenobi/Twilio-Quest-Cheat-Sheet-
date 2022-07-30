8fb8178394dca42fdcac2e4a2a6351cf46f33f91
Harry Potter ;D <109922250+NobieWanKenobi@users.noreply.github.com>
2022-07-29 22:00:53 -0500

    Add files via upload

---
+++ chestConfiguration.js
@@ -0,0 +1,7 @@
+const verifiedUser = Boolean ("True");
+const accessLevel = Number("7");
+const favoriteRobot = "Cedric";
+
+console.log ('The user is online?' + verifiedUser);
+console.log('What is the access level?' + accessLevel);
+console.log('Who is your favorite robot?' + favoriteRobot);
